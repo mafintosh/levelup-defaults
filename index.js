3a8de5887ab044db455a6c4c2ab56ae5532a3e36
Mathias Buus <mathiasbuus@gmail.com>
2015-01-17 15:49:35 -0800

    do not open leveldown twice

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 var levelup = require('levelup')
 
+var noop = function() {}
+
+var Proxy = function(db) {
+  this.db = db
+  this.leveldown = null
+}
+
+Proxy.prototype.open = function(opts, cb) {
+  if (typeof opts === 'function') return this.open(null, opts)
+  if (!cb) cb = noop
+  var self = this
+  this.db.open(function(err) {
+    if (err) return cb(err)
+    self.leveldown = self.db.db
+    cb()
+  })
+}
+
+'put get del batch approximateSize iterator close'.split(' ').forEach(function(m) {
+  Proxy.prototype[m] = function() {
+    return this.leveldown[m].apply(this.leveldown, arguments)
+  }
+})
+
 module.exports = function(db, defaults) {
   if (!defaults) defaults = {}
 
   defaults.db = function(l) {
-    var down = db.db
-    db.on('open', function() {
-      up.db = db.db
-    })
-    return down
+    return new Proxy(db)
   }
 
   var up = levelup(db.location || 'no-location', defaults)
+
+  up.on('ready', function() { // remove the proxy
+    if (up.db instanceof Proxy) up.db = up.db.leveldown
+  })
 
   return up
 }
