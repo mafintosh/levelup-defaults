30f0afa7d9208fd3808180a31d048fb5283f3423
Mathias Buus <mathiasbuus@gmail.com>
2015-01-17 14:18:07 -0800

    workaround deferreddown

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var levelup = require('levelup')
 module.exports = function(db, defaults) {
   if (!defaults) defaults = {}
 
-  defaults.db = function() {
-    return db.db
+  defaults.db = function(l) {
+    var down = db.db
+    db.on('open', function() {
+      up.db = db.db
+    })
+    return down
   }
 
-  return levelup(db.location || 'no-location', defaults)
+  var up = levelup(db.location || 'no-location', defaults)
+
+  return up
 }
